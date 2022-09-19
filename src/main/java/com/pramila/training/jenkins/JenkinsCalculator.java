882bb45960bbca88ec35d56525651d2bba9d7d16
Halyal <2213370@cognizant.com>
2022-09-19 20:35:21 +0530

    first commit to the project

---
+++ src/main/java/com/pramila/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,20 @@
+package com.pramila.training.jenkins;
+
+public class JenkinsCalculator {
+
+	//Method to add 2 numbers
+	
+	public int addNumbers(int numOne,int numTwo) {
+		
+		int var1=20;
+		System.out.println("this is a sample test");
+		return numOne + numTwo;
+	}
+	
+	//Method to subtract 2 numbers
+	
+		public int subtractNumbers(int numOne,int numTwo) {
+			
+			return numOne - numTwo;
+		}
+}
---
+++ src/test/java/com/pramila/training/jenkins/JenkinksCalctest.java
@@ -0,0 +1,20 @@
+package com.pramila.training.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinksCalctest {
+
+	@Test
+	public void addTest() {
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(10,myCalc.addNumbers(5, 5));	
+	}
+
+	@Test
+	public void subtractTest() {
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(5,myCalc.subtractNumbers(10, 5));	
+	}
+}
